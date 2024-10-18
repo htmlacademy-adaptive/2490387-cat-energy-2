a19f4fb5d29f2db91bb6988f984d3eff9d5dd4f2
alexsoserdotskiy <soserdotskiy@gmail.com>
2024-10-18 10:20:30 +0300

    change header

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,9 @@
-/* в этот файл добавляет скрипты*/
+const nav = document.querySelector('.nav');
+const navToggle = document.querySelector('.nav__toggle');
+
+nav.classList.add('nav--closed');
+
+navToggle.addEventListener('click', ()=>{
+    nav.classList.toggle('nav--closed');
+    nav.classList.toggle('nav--opened');
+});
